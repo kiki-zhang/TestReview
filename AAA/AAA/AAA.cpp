1769dda64ae8db305bb38b960168e6e0ad832320
LianweiHuang <Lianwei.Huang@autodesk.com>
2014-12-02 15:45:03 +0800

    added project
    added the first project

---
+++ AAA/AAA/AAA.cpp
@@ -0,0 +1,13 @@
+// AAA.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+   cout << "hello world!" << endl;
+	return 0;
+}
+
